64a78465992d50e05a4202b103256d2106c8eec8
BrktrL <74716514+Brktrlw@users.noreply.github.com>
2021-10-17 13:33:03 +0300

    Update

--- blog/article/admin.py
+++ blog/article/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Article
 
-admin.site.register(Article)
+#admin.site.register(Article)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["title","author","createdDate"]
+    class Meta:
+        model=Article
+
 # Register your models here.
