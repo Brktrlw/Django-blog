26fca4171b2b6c8178859bec121b6d90f2ee5b32
BrktrL <74716514+Brktrlw@users.noreply.github.com>
2021-10-18 12:35:51 +0300

    Update

--- blog/article/views.py
+++ blog/article/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,"index.html")
+    return render(request,"article/index.html")
 def about(request):
-    return render(request,"about.html")
+    return render(request,"article/about.html")
