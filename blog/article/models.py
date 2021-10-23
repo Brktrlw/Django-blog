96de2762be4e519c5f81bbc9aa9e483a333eb7bb
BrktrL <74716514+Brktrlw@users.noreply.github.com>
2021-10-23 17:42:44 +0300

    Update

--- blog/article/models.py
+++ blog/article/models.py
@@ -4,6 +4,8 @@ class ArticleType(models.Model):
     articleType=models.CharField(max_length=50)
     def __str__(self):
         return self.articleType
+
+
 class Article(models.Model):
     author     = models.ForeignKey("auth.User",on_delete=models.CASCADE,verbose_name="Yazar")
     title      = models.CharField(max_length=50,verbose_name="Başlık")
@@ -14,6 +16,14 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+class Comment(models.Model):
+    article=models.ForeignKey(Article,on_delete=models.CASCADE,related_name="comments")
+    commentAuthor=models.ForeignKey("auth.User",on_delete=models.CASCADE)
+    commentContent=models.CharField(max_length=200)
+    commentDate=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.commentContent
 
 
 
